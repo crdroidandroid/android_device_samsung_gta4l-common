20bf6eb7489e58a662791b9f284aabcac180a9f3
Suren Baghdasaryan <surenb@google.com>
2022-07-18 17:04:06 -0700

    gta4l-common: Disable per-cgroup PSI accounting
    Per-cgroup PSI accounting is not used in Android while having small
    performance cost. Disable this unused feature.
    
    Bug: 233410456
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: I9e8fb209905f9873926754da8c8e295c1655aa8d
    Merged-In: I9e8fb209905f9873926754da8c8e295c1655aa8d
    [jjpprrrr: Note that this feature requires a backported kernel commit]
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -107,6 +107,7 @@ BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x4a90
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048 loop.max_part=7 firmware_class.path=/vendor/firmware_mnt/image
+BOARD_KERNEL_CMDLINE += cgroup_disable=pressure
 
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_OFFSET := 0x00008000
