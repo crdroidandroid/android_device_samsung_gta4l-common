1071e256318aff3a6f70412247a40fea0e0ba236
Michael Bestas <mkbestas@lineageos.org>
2024-03-21 13:38:44 +0200

    gta4l-common: Switch to android.hardware.usb@1.3-service.dual_role_usb
    Change-Id: Ibd14f7d31dfaf41e040302eda3bc49f351deb618

--- gta4l.mk
+++ gta4l.mk
@@ -332,7 +332,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb@1.3-service.dual_role_usb
 
 # VNDK
 PRODUCT_COPY_FILES += \
