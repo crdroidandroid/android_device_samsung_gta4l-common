dd1641687aafadd1ee35da3a796732b337241975
CuriousNom <aryanbiswas7585@gmail.com>
2024-07-17 19:19:21 +0530

    gta4l-common: Enable Build Signing

--- gta4l.mk
+++ gta4l.mk
@@ -23,6 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/samsung/gta4l-common/gta4l-common-vendor.mk)
 
+# Build Signer
+$(call inherit-product-if-exists, vendor/lineage-priv/product.mk)
+
 # Tablet
 PRODUCT_CHARACTERISTICS := tablet
 
